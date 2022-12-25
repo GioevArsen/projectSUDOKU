76ee4c3d1a162cf142e963e4b21a8d827f61b678
Арсен Гиоев <gioev.arsen15@list.ru>
2022-12-25 03:10:50 +0300

    Добавьте файлы проекта.

---
+++ projectSUDOKU/FormGame.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace projectSUDOKU
+{
+    partial class FormGame
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormGame));
+            this.dataGridViewLevel = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewLevel)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewLevel
+            // 
+            this.dataGridViewLevel.AllowUserToAddRows = false;
+            this.dataGridViewLevel.AllowUserToDeleteRows = false;
+            this.dataGridViewLevel.AllowUserToResizeColumns = false;
+            this.dataGridViewLevel.AllowUserToResizeRows = false;
+            this.dataGridViewLevel.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewLevel.ColumnHeadersVisible = false;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridViewLevel.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridViewLevel.Location = new System.Drawing.Point(12, 12);
+            this.dataGridViewLevel.Name = "dataGridViewLevel";
+            this.dataGridViewLevel.ReadOnly = true;
+            this.dataGridViewLevel.RowHeadersVisible = false;
+            this.dataGridViewLevel.Size = new System.Drawing.Size(240, 150);
+            this.dataGridViewLevel.TabIndex = 0;
+            this.dataGridViewLevel.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewLevel_CellClick);
+            this.dataGridViewLevel.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridViewLevel.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridViewLevel_CellMouseClick);
+            //this.dataGridViewLevel.KeyDown += new System.Windows.Forms.KeyEventHandler(this.dataGridViewLevel_KeyDown);
+            // 
+            // FormGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridViewLevel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormGame";
+            this.Text = "СУДОКУ";
+            this.Load += new System.EventHandler(this.FormGame_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewLevel)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewLevel;
+    }
+}
+
---
+++ projectSUDOKU/FormGame.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace projectSUDOKU
+{
+    public partial class FormGame : Form
+    {
+        int[,] field;
+        int[,] field1;  // для проверки на пропавшее число
+        int[,] field2; // игровое поле, которое заполняется
+        int CellSize = 40;
+        Random rnd = new Random();
+        
+        
+        public FormGame()
+        {
+            InitializeComponent();
+        }
+
+
+        
+        void CreateLevel()
+        {
+            dataGridViewLevel.RowTemplate.Height = CellSize;
+            for (int i = 0; i < field.GetLength(1); i++)
+            {
+                dataGridViewLevel.Columns.Add("", "");
+                dataGridViewLevel.Columns[i].Width = CellSize;
+            }
+            dataGridViewLevel.Rows.Add(field.GetLength(0));
+            dataGridViewLevel.Width = field.GetLength(1) * (CellSize) + 3;
+            dataGridViewLevel.Height = field.GetLength(0) * CellSize + 3;
+        }
+
+
+        void ShowLevel()
+        {
+            for(int i = 0; i < field.GetLength(0); i++)
+            {
+                for(int j = 0; j < field.GetLength(1); j++)
+                {
+                    field1[i, j] = field[i, j];
+                    dataGridViewLevel.Rows[i].Cells[j].Value = field[i, j];
+                    dataGridViewLevel.Rows[i].Cells[j].Style.BackColor = Color.FromArgb(192, 192, 192);
+                    dataGridViewLevel.Rows[i].Cells[j].Style.ForeColor = Color.Black;
+                    
+                }
+            }
+            for (int i = 0; i < field.GetLength(0); i++)
+            {
+                for (int x = 0; x < 4; x++)
+                {
+                    int j = rnd.Next(0, 8);
+                    dataGridViewLevel.Rows[i].Cells[j].Value = "";
+                    dataGridViewLevel.Rows[i].Cells[j].Style.BackColor = Color.FromArgb(220, 220, 220);
+                    dataGridViewLevel.Rows[i].Cells[j].Style.ForeColor = Color.Orange;
+                    field1[i, j] = 0;
+                }
+            }
+            for (int j = 0; j < field.GetLength(1); j++)
+            {
+                for (int x = 0; x < 4; x++)
+                {
+                    int i = rnd.Next(0, 8);
+                    dataGridViewLevel.Rows[i].Cells[j].Value = "";
+                    dataGridViewLevel.Rows[i].Cells[j].Style.BackColor = Color.FromArgb(220, 220, 220);
+                    dataGridViewLevel.Rows[i].Cells[j].Style.ForeColor = Color.Orange;
+                    field1[i, j] = 0;
+                }
+            }
+        }
+        
+        
+
+        
+        void LoadLevel()
+        {
+            StreamReader str = new StreamReader("level.txt");
+            string[] temp = str.ReadLine().Split();
+            int n = Convert.ToInt32(temp[0]);
+            int m = Convert.ToInt32(temp[1]);
+            field = new int[n, m];
+            field1 = new int[n, m];
+            field2 = new int[n, m];
+            for(int i = 0; i < n; i++)
+            {
+                string temp1 = str.ReadLine();
+                for(int j = 0; j < m; j++)
+                {
+                    if (Char.IsDigit(temp1[j]))
+                    {
+                        field[i, j] = temp1[j] - 48;
+                    }
+                    field2[i, j] = field[i, j];
+                }
+            }
+            str.Close();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void FormGame_Load(object sender, EventArgs e)
+        {
+            LoadLevel();
+            CreateLevel();
+            ShowLevel();
+        }
+
+        private void dataGridViewLevel_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int clickRow = e.RowIndex;
+            int clickColumn = e.ColumnIndex;
+
+            //UserEnter(clickRow, clickColumn);
+        }
+
+        
+
+        /*private void dataGridViewLevel_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.NumPad3)
+            {
+                dataGridViewLevel.SelectedCells[0].Value = 3;
+            }
+
+        }*/
+
+        private void dataGridViewLevel_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            int row = e.RowIndex;
+            int column = e.ColumnIndex;
+            if (field1[row, column] != 0)
+            {
+                dataGridViewLevel.CancelEdit();
+            }
+            else
+            {
+
+                if (Convert.ToString(dataGridViewLevel.Rows[row].Cells[column].Value) == "")
+                {
+                    dataGridViewLevel.Rows[row].Cells[column].Value = 1;
+                    field2[row, column] = 1;
+                }
+                else
+                {
+                    int content = Convert.ToInt32(dataGridViewLevel.Rows[row].Cells[column].Value);
+                    if (content == 9)
+                    {
+                        dataGridViewLevel.Rows[row].Cells[column].Value = 1;
+                        field2[row, column] = 1;
+                    }
+                    else
+                    {
+                        dataGridViewLevel.Rows[row].Cells[column].Value = content + 1;
+                        field2[row, column] = content + 1;
+                    }
+                }
+            }
+        }
+    }
+}
